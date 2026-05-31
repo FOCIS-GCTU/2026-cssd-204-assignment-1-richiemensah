715a0b5af51796c0767d8c5e81527f21dbd90612
richiemensah <2425403127@live.gctu.edu.gh>
2026-05-31 13:49:58 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# file: <initials .py>
+# Descirption : <print out the initials of my name>
+# Assignment Number: <Assignment number 1>
+#
+#Name:< Richmond Mensah Fabin>
+#STUDENT ID: <2425403127>
+# Email: <2425403127@live.gctu.edu.gh>
+# Grader: < Augustus >
+#
+# On my honor, < RICHMOND MENSAH FABIN > This programming assignment is my own work
+# and i have not provided this code to any other student
+
+
 
 def main():
-  pass #code goes here
+   # Prints initials R, M, F in large block letters
 
+   print()
+   print("...RMF")
+   print()
+   print("...RRRRRRRRRR..........MM........MM........FFFFFFFFFFFF.....")
+   print("...RR........RR........MMM......MMM........FF...............")
+   print("...RR........RR........MMMM....MMMM........FF...............")
+   print("...RRRRRRRRRR..........MM.MM..MM.MM........FFFFFFFF.........")
+   print("...RR...RR.............MM..MMMM..MM........FF...............")
+   print("...RR....RR............MM...MM...MM........FF...............")
+   print("...RR.....RR...........MM........MM........FF...............")
+   print("...RR......RR..........MM........MM........FF...............")
+   print("...RR.......RR....**...MM........MM...**...FF.............**")
+   print("...RR........RR...**...MM........MM...**...FF.............**")
+   print()
 
 
 main()
+
+
+
+
